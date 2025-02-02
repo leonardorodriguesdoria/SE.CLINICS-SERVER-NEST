77bec6c95cb22a16d5d5f8f90eb110ef3198c636
Leonardo Rodrigues <leordoria2016@gmail.com>
2025-02-02 20:40:35 -0300

    Folter name correction - users to patient

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { PatientModule } from './patient/patient.module';
 import { ProfessionalModule } from './professional/professional.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { ClinicModule } from './clinic/clinic.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const databasePort = process.env.DB_PORT as unknown as number | undefined;
 
 @Module({
-  imports: [AuthModule, UsersModule, ProfessionalModule, AppointmentModule, ClinicModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: databasePort,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    }),
+    AuthModule,
+    ProfessionalModule,
+    PatientModule,
+    AppointmentModule,
+    ClinicModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
