4cd5d7492e69784504430253581378693d3d9674
Leonardo Rodrigues <leordoria2016@gmail.com>
2025-02-02 19:43:08 -0300

    Folder Structure Tree

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProfessionalModule } from './professional/professional.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import { ClinicModule } from './clinic/clinic.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProfessionalModule, AppointmentModule, ClinicModule],
   controllers: [AppController],
   providers: [AppService],
 })
